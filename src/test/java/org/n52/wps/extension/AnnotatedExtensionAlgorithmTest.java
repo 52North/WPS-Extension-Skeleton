9ecac6184421ec08d5398d7f98d9bb6fa6a92f25
matthesrieke <m.rieke@52north.org>
2013-05-17 14:56:09 +0200

    add an annotated algorithm. do some logging in unit tests.

--- src/test/java/org/n52/wps/extension/AnnotatedExtensionAlgorithmTest.java
+++ src/test/java/org/n52/wps/extension/AnnotatedExtensionAlgorithmTest.java
@@ -36,19 +36,26 @@ import org.n52.wps.server.ExceptionReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExtensionAlgorithmTest {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ExtensionAlgorithmTest.class);
+public class AnnotatedExtensionAlgorithmTest {
 
+	private static final Logger logger = LoggerFactory.getLogger(AnnotatedExtensionAlgorithmTest.class);
+	
 	@Test
 	public void testAlgo() throws ExceptionReport {
 		Map<String, List<IData>> map = readData();
 		
-		ExtensionAlgorithm algo = new ExtensionAlgorithm();
+		AnnotatedExtensionAlgorithm algo = new AnnotatedExtensionAlgorithm();
 		Map<String, IData> result = algo.run(map);
 		
-		Assert.assertTrue("Result not available!", result.get(ExtensionAlgorithm.OUTPUT) != null);
-		logger.info("Succesfully retrieved result: '{}'", result.get(result.get(ExtensionAlgorithm.OUTPUT)));
+		Assert.assertTrue("Result not available!", result.get(resolveOutputIdentifier(algo)) != null);
+		logger.info("Succesfully retrieved result: '{}'", result.get(resolveOutputIdentifier(algo)));
+	}
+
+	private String resolveOutputIdentifier(AnnotatedExtensionAlgorithm algo) {
+		String id = algo.getDescription().getProcessOutputs().getOutputArray(0)
+				.getIdentifier().getStringValue();
+		logger.info("Output identifier is '{}'", id);
+		return id;
 	}
 
 	private Map<String, List<IData>> readData() {
