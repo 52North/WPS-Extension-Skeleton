65f7f0ddc1d2a70ef32f428ec5fc21c43322caab
Benjamin Pross <b.pross@52north.org>
2017-08-04 15:38:19 +0200

    initial import

---
+++ src/main/java/testbed13/dsi/ConflationWorkflow.java
@@ -0,0 +1,36 @@
+package testbed13.dsi;
+
+import java.util.List;
+import java.util.Map;
+
+import org.n52.wps.io.data.IData;
+import org.n52.wps.server.AbstractAlgorithm;
+import org.n52.wps.server.ExceptionReport;
+
+public class ConflationWorkflow extends AbstractAlgorithm {
+
+    @Override
+    public List<String> getErrors() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Class<?> getInputDataType(String arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Class<?> getOutputDataType(String arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<String, IData> run(Map<String, List<IData>> arg0) throws ExceptionReport {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
